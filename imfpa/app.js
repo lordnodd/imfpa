23ebbb3563d05af9af02eee82fb6f13405681b45
lordnodd <ashwindyadav1987@gmail.com>
2019-07-26 15:02:19 +0530

    Initial Commit

--- imfpa/app.js
+++ imfpa/app.js
@@ -2,56 +2,32 @@ var Jimp = require('jimp');
 var path = require('path');
 //if you are following along, create the following 2 images relative to this script:
 
-let imgRaw = path.join(__dirname, 'raw/1.0-inch-frame_Natural.png'); //a 1024px x 1024px backgroound image
-let imgLogo = path.join(__dirname,'raw/logo.png'); //a 155px x 72px logo
+let frame = path.join(__dirname, 'raw/1.0-inch-frame_Natural.png'); // frame's path
+let painting = path.join(__dirname,'raw/logo.png'); // painting's path
 //
-console.log(imgRaw);
-console.log(imgLogo);
+console.log(frame);
+console.log(painting);
 
-let imgActive = 'active/image.jpg';
-let imgExported = 'export/image1.jpg';
+let imgActive = 'active/image.jpg'; // cloned image for operations
+let imgExported = 'export/image1.jpg'; // output file 
 
-let textData = {
-  text: '© Bit Brothers Pvt Ltd', //the text to be rendered on the image
-  maxWidth: 1004, //image width - 10px margin left - 10px margin right
-  maxHeight: 72+20, //logo height + margin
-  placementX: 600, // 10px in on the x axis
-  placementY: 1350-(72+20)-10 //bottom of the image: height - maxHeight - margin 
-};
 
 //read template & clone raw image 
-Jimp.read(imgRaw)
+Jimp.read(frame)
   .then(tpl => {
     tpl.autocrop().resize(1080, Jimp.AUTO).clone().write(imgActive);
   })
+  
   //read cloned (active) image
   .then(() => (Jimp.read(imgActive)))
 
   //combine logo into image
   .then(tpl => (
-    Jimp.read(imgLogo).then(logoTpl => {
-      logoTpl.opacity(1).resize(872,700);
-      return tpl.composite(logoTpl, 108, 100, [Jimp.BLEND_DESTINATION_OVER, 1, 1]);
+    Jimp.read(painting).then(paintTpl => {
+      paintTpl.opacity(1).resize(872,700);
+      return tpl.composite(paintTpl, 108, 100, [Jimp.BLEND_DESTINATION_OVER, 1, 1]);
     })
   ))
-
-  // //load font	
-  // .then(tpl => (
-  //   Jimp.loadFont(Jimp.FONT_SANS_32_WHITE).then(font => ([tpl, font]))
-  // ))
-	
-  //add footer text
-  // .then(data => {
-  //   // console.log(data);
-  //   tpl = data[0];
-  //   font = data[1];
-  
-  //   return tpl.print(font, textData.placementX, textData.placementY, {
-  //     text: textData.text,
-  //     alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
-  //     alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE
-  //   }, textData.maxWidth, textData.maxHeight);
-  // })
 
   //export image
   .then(tpl => (tpl.quality(100).write(imgExported)))
